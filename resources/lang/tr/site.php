1bb7956e3e636611b9bf8981a9fd7cc6158d58fc
Zeynallow <zeynaloffnet@gmail.com>
2019-11-30 10:54:02 +0400

    v5.7

--- resources/lang/tr/site.php
+++ resources/lang/tr/site.php
@@ -1,53 +1,37 @@
 <?php
 
 return array (
-  //Home
-  'home' => 'Ana sayfa',
+  'admin_panel' => 'Admin panel',
   'categories' => 'Kategoriler',
-  'popular_posts' => 'Popüler içerikler',
-
-  //Posts
-  'no_posts' => 'İçerikler bulunamadı',
+  'comments' => 'Yorumlar',
+  'confirm_password' => 'Şifreyi Onayla',
+  'email' => 'E-mail',
+  'forgot_password' => 'Parolanızı mı unuttunuz',
+  'home' => 'Ana sayfa',
   'last_posts' => 'Son içerikler',
+  'leave_a_comment' => 'Yorum Yap',
+  'leave_empty_to_keep_the_same' => 'Aynı tutmak için boş bırakın',
+  'logout' => 'Çıkış Yap',
+  'name' => 'Ad',
+  'no_posts' => 'İçerikler bulunamadı',
+  'no_result' => 'Sonuç yok',
+  'or' => 'veya',
+  'password' => 'Şifre',
+  'please_confirm_your_password_before_continuing' => 'Lütfen devam etmeden önce şifrenizi onaylayın.',
+  'please_login_website' => 'Lütfen giriş yapın',
+  'popular_posts' => 'Popüler içerikler',
+  'post_a_comment' => 'Yorum Gönder',
+  'profile' => 'Profil',
   'read_more' => 'daha fazla',
   'reply' => 'Cevapla',
-  'comments' => 'Yorumlar',
-  'leave_a_comment' => 'Yorum Yap',
-  'post_a_comment' => 'Yorum Gönder',
-  'your_comment' => 'Senin yorumun',
-
-  //Profile
-  'update_profile' => 'Profili Güncelle',
-  'name' => 'Ad',
-  'email' => 'E-mail',
-  'password' => 'Şifre',
-  'leave_empty_to_keep_the_same' => 'Aynı tutmak için boş bırakın',
-  'confirm_password' => 'Şifreyi Onayla',
-  'update' => 'Güncelle',
-
-  //Search
-  'search_result' => 'Arama sonuçları',
-  'no_result' => 'Sonuç yok',
-
-  //Footer
-  'social_networks' => 'Sosyal ağlar',
-
-  //Auth
-  'sign_in' => 'Oturum aç',
-  'forgot_password' => 'Parolanızı mı unuttunuz',
-  'or' => 'veya',
-  'sign_up' => 'Kaydol',
   'reset_password' => 'Şifreyi yenile',
+  'search_result' => 'Arama sonuçları',
   'send_password_reset_link' => 'Parola Sıfırlama Bağlantısını Gönder',
-  'please_confirm_your_password_before_continuing' => 'Lütfen devam etmeden önce şifrenizi onaylayın.',
-
-  //Alert
+  'sign_in' => 'Oturum aç',
+  'sign_up' => 'Kaydol',
+  'social_networks' => 'Sosyal ağlar',
   'something_went_error' => 'Bir şeyler yanlış gitti ...',
-  'please_login_website' => 'Lütfen giriş yapın',
-
-  // Menu
-  'admin_panel' => 'Admin panel',
-  'profile' => 'Profil',
-  'logout' => 'Çıkış Yap',
-
-  );
+  'update' => 'Güncelle',
+  'update_profile' => 'Profili Güncelle',
+  'your_comment' => 'Senin yorumun',
+);
