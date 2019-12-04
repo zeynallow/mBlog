005fbcc334e27cadc58060ac1f48e56708a701cd
Zeynallow <zeynaloffnet@gmail.com>
2019-12-04 14:35:20 +0400

    7.0.4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,20 +26,20 @@ class AppServiceProvider extends ServiceProvider
   {
 
     //load config
-    Config::set('app.name',getSetting('site_name'));
-
-    if(getDefaultLocale()){
-      Config::set('app.locale',getDefaultLocale()->code);
-    }
-
-    Config::set('app.timezone',getSetting('timezone'));
-    Config::set('app.url',getSetting('site_url'));
-    Config::set('mail.host',getSetting('email_host'));
-    Config::set('mail.port',getSetting('email_port'));
-    Config::set('mail.username',getSetting('email_username'));
-    Config::set('mail.password',getSetting('email_password'));
-    Config::set('mail.from.address',getSetting('email_address'));
-    Config::set('mail.from.name',getSetting('email_title'));
+    // Config::set('app.name',getSetting('site_name'));
+    //
+    // if(getDefaultLocale()){
+    //   Config::set('app.locale',getDefaultLocale()->code);
+    // }
+    //
+    // Config::set('app.timezone',getSetting('timezone'));
+    // Config::set('app.url',getSetting('site_url'));
+    // Config::set('mail.host',getSetting('email_host'));
+    // Config::set('mail.port',getSetting('email_port'));
+    // Config::set('mail.username',getSetting('email_username'));
+    // Config::set('mail.password',getSetting('email_password'));
+    // Config::set('mail.from.address',getSetting('email_address'));
+    // Config::set('mail.from.name',getSetting('email_title'));
 
   }
 
